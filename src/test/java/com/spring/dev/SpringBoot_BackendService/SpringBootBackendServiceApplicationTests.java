8e6611dd29e64c9941e8f1604d92a2ba586b0711
tusharanand7893 <tushar.anand7893@gmail.com>
2023-03-25 00:50:32 +0530

    backendSpringFramework Setup

---
+++ src/test/java/com/spring/dev/SpringBoot_BackendService/SpringBootBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.dev.SpringBoot_BackendService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
