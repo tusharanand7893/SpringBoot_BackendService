914c2fe7f0269e8aeda753c3de87ffba0c71ec9d
tusharanand7893 <tushar.anand7893@gmail.com>
2023-03-26 01:06:08 +0530

    Added API for fetching all books and book by id

--- src/main/java/com/spring/dev/controller/LibraryController.java
+++ src/main/java/com/spring/dev/controller/LibraryController.java
@@ -4,13 +4,15 @@ import com.spring.dev.dbEntity.LibraryDao;
 import com.spring.dev.entities.AddBookResponse;
 import com.spring.dev.service.LibraryService;
 import com.spring.dev.utility.LibraryRepository;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 public class LibraryController {
@@ -41,5 +43,20 @@ public class LibraryController {
         }
         return new ResponseEntity<AddBookResponse>(addBookResponse,httpHeaders,httpStatus);
 
+    }
+
+    @GetMapping("/api/getBookById/{id}")
+    public LibraryDao getBookById(@PathVariable(value = "id") String id){
+        try {
+            return libraryRepository.findById(id).get();
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("api/getAllBooks")
+    public List<LibraryDao> getAllBooks(){
+        return libraryRepository.findAll();
     }
 }
